c07631b79b43ccfc090165478ce0b9beb2ada87f
Maciel Giroletti Mottin <51345526+macimottin@users.noreply.github.com>
2023-12-29 19:32:12 -0300

    Update Trend_History.py

--- Lambda_Functions/Trend_History.py
+++ Lambda_Functions/Trend_History.py
@@ -20,8 +20,6 @@ def check_and_append_data(latest_trend_df, trend_hist_df):
         updated_trend_hist_df = pd.concat([trend_hist_df, new_data_df])
         return updated_trend_hist_df, "History updated with latest trends."
 
-status_message, updated_trend_hist_df.head()
-
 def lambda_handler(event, context):
     # Specify your S3 bucket and file details
     bucket = 'asset.genie'
@@ -34,13 +32,13 @@ def lambda_handler(event, context):
     df2 = wr.s3.read_csv(f's3://{bucket}/{input_file_2}')
   
     # Check for new data and append if necessary
-    updated_trend_hist_df = check_and_append_data(df1, df2)
+    updated_trend_hist_df, status_message = check_and_append_data(df1, df2)
 
     # Write the updated dataframe to a new CSV in S3
     wr.s3.to_csv(updated_trend_hist_df, f's3://{bucket}/{output_file_key}', index=False)
 
     return {
                 'statusCode': 200,
-                'body': f'Trend History was updated sucessfully.'
+                'body': status_message
             }
 

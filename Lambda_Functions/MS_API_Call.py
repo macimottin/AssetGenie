d0c5c1d59bbfc906acef699b7fccabb947bfbd0c
Maciel Giroletti Mottin <51345526+macimottin@users.noreply.github.com>
2023-12-29 16:50:07 -0300

    Update MS_API_Call.py

--- Lambda_Functions/MS_API_Call.py
+++ Lambda_Functions/MS_API_Call.py
@@ -1,1 +1,56 @@
+import requests
+import awswrangler as wr
+import pandas as pd
+from datetime import datetime, timedelta
 
+def lambda_handler(event, context):
+
+    # Your API key
+    api_key = '088eaf16b9832d99695bda9073fa92e9'
+
+    # Define the file path within AWS S3 for reading
+    raw_s3_bucket = 'asset.genie'
+    raw_path_dir = 'scout/IBOV.csv'
+    raw_path = f"s3://{raw_s3_bucket}/{raw_path_dir}"
+
+    # Load stock tickers from CSV using awswrangler
+    stock_tickers_df = wr.s3.read_csv(raw_path, sep=';')
+    stock_tickers = stock_tickers_df['code'].tolist()
+
+    # Date range
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=60)).strftime('%Y-%m-%d')
+
+    # Fetch stock data
+    stock_data = fetch_stock_data(api_key, stock_tickers, start_date, end_date)
+
+    # Send final DataFrame to S3 bucket using awswrangler
+    output_s3_path = "s3://asset.genie/scout/ibov_data.csv"
+    wr.s3.to_csv(stock_data, output_s3_path, index=False)
+
+    return "Data processing completed successfully"
+
+def fetch_stock_data(api_key, tickers, start_date, end_date):
+    base_url = 'http://api.marketstack.com/v1/eod'
+    all_stocks_data = pd.DataFrame()
+
+    for ticker in tickers:
+        # Append .BVMF to each ticker symbol
+        formatted_ticker = f"{ticker}.BVMF"
+        
+        params = {
+            'access_key': api_key,
+            'symbols': formatted_ticker,
+            'date_from': start_date,
+            'date_to': end_date
+        }
+        response = requests.get(base_url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            stock_data = pd.json_normalize(data, 'data')
+            all_stocks_data = pd.concat([all_stocks_data, stock_data], ignore_index=True)
+        else:
+            print(f"Failed to fetch data for {formatted_ticker}. Status Code: {response.status_code}")
+            print("Response:", response.text)
+
+    return all_stocks_data
